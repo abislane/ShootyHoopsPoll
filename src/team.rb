103cb91a442881cb9aa53a51ac2e3820de48992a
Matthew Slane <mcslane@iastate.edu>
2016-11-04 23:07:29 -0500

    added team data structure

---
+++ src/team.rb
@@ -0,0 +1,25 @@
+module Team
+  class Team
+    attr_reader :name :num_games :num_wins :score :rank
+    attr_writer :name :num_games :num_wins :score :rank
+
+    def initialize(name)
+      @name = name
+      @num_games = 0
+      @num_wins = 0
+      @score = 0
+      @rank = 1
+    end
+
+    def add_game(won, points, opp_rank)
+      @num_games += 1
+      if(won)
+        @games_won += 1
+      end
+    end
+
+    def to_s
+      @name + " has won " + @num_wins +" out of " + num_games
+    end
+  end
+end
